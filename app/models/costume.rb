fdba09c4e8abfbd3c50796aca36b2885ac39e7e2
Susana <susana.m.vik@gmail.com>
2021-03-07 20:35:26 -0500

    commit tree c0ff190a52127571de1d45790f1fc09490ae0e9b

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,2 +1,8 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
+
+class Costume < ActiveRecord::Base
+
+end
+
+#Costume.create(:name => "Milky Way Midnight", :calories => 220)
